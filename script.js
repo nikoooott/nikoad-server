73189eec100d15610f53d634ea705091d837f2e4
nikoooott <nikobabayan777@mail.ru>
2025-06-28 05:12:46 +0400

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById('orderForm').addEventListener('submit', async e => {
   status.style.color = 'black';
 
   try {
-    const res = await fetch('https://nikoad-server.cyclic.app/api/send', {
+    const res = await fetch('https://nikoad-backend.fly.dev/api/send', { // <== мой сервер
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ name, tg, msg }),
